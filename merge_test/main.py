b0c440c1c52c96bbba5cb90f7b831a81e8265925
Analise Mengarelli <tuu45673@temple.edu>
2025-02-20 18:24:02 +0000

    Adding square root function

--- merge_test/main.py
+++ merge_test/main.py
@@ -1,4 +1,4 @@
-from utils import add, multiply
+from utils import add, multiply, sqrt
 
 
 def main():
@@ -6,5 +6,8 @@ def main():
     result = multiply(80.5, 33)
     print(f"The result of multiplication is {result}")
 
+    n = 144
+    result = sqrt(n)
+    print(f"The square root of {n} equals {result}")
 
 main()
--- merge_test/utils.py
+++ merge_test/utils.py
@@ -1,9 +1,25 @@
+import math
+
 def add(a, b):
+    """
+    Adds two numbers
+    """
     res = a + b
     return res
 
 def multiply(a, b):
+    """
+    Multiplies two numbers
+    """
     res = a * b 
     return res
 
-print("You are into utils.py")
+def sqrt(a):
+    """
+    Calculates square root of a using build-in math library
+    """
+    result = math.sqrt(a)
+    return result
+
+
+# print("You are into utils.py")
